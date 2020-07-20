713d9fadbe0078704c19132c135c1ef71c8ff00b
75ks <kenta.s705@gmail.com>
2020-07-20 14:54:37 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :room_user
-  has_many :user, through: :room_user
-  has_many :message
+  has_many :user, through: :room_user, dependent: :destroy
+  has_many :message, dependent: :destroy
   validates :name, presence: true
 end
